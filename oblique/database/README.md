2ea58e8e464e975b7c65451fe05d7de1a545df41
Astariul <remondnicola@gmail.com>
2023-09-15 23:45:45 +0900

    📝 Add DB schema

---
+++ oblique/database/README.md
@@ -0,0 +1,5 @@
+<p align="center">
+    <a href="https://docs.google.com/drawings/d/1vmC5mxb8ZM2CWrsxZSoUUGmPiAc0k8Uj2EaN3vFQMaQ/edit?usp=sharing"><img src="https://img.shields.io/badge/-Google%20drawing-red" alt="Google drawing link" /></a>
+</p>
+
+<img src="https://gist.githubusercontent.com/astariul/4676386fa61917697a2c8cf5af267e19/raw/8cca8e83c0dc1f64d2d90e8dcb9cdf4c127fe350/db.svg">
--- oblique/database/models.py
+++ oblique/database/models.py
@@ -5,61 +5,26 @@ from sqlalchemy.orm import relationship
 from oblique.database import Base
 
 
-class Integration(Base):
-    """Table to represent a Notion Integration."""
+class Package(Base):
+    """Table representing a Python Package registered in PyPi."""
 
-    __tablename__ = "integrations"
-
-    id = Column(Integer, primary_key=True, index=True)
-    token = Column(String, unique=True, index=True)
-
-    tables = relationship("Table", back_populates="integration")
-
-
-class Table(Base):
-    """Table to represent a Notion Table (a Notion DB)."""
-
-    __tablename__ = "tables"
+    __tablename__ = "packages"
 
     id = Column(Integer, primary_key=True, index=True)
-    table_id = Column(String, index=True)
-    integration_id = Column(Integer, ForeignKey("integrations.id"))
+    last_updated = Column(DateTime)
+    name = Column(String, unique=True, index=True)
 
-    integration = relationship("Integration", back_populates="tables")
-    elements = relationship("Element", back_populates="table")
-
-
-class Element(Base):
-    """Table to represent an element in a Notion Table (basically a row)."""
-
-    __tablename__ = "elements"
-
-    id = Column(Integer, primary_key=True, index=True)
-    last_edited = Column(DateTime)
-    notion_id = Column(String, unique=True, index=True)
-    table_id = Column(Integer, ForeignKey("tables.id"))
-
-    table = relationship("Table", back_populates="elements")
-    attributes = relationship("Attribute", back_populates="element", passive_deletes=True)
+    releases = relationship("Release", back_populates="integration", passive_deletes=True)
 
 
-class Attribute(Base):
-    """Table to represent a single attribute of a row of a Notion DB."""
+class Release(Base):
+    """Table representing a single release of a python package."""
 
-    __tablename__ = "attributes"
+    __tablename__ = "releases"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False)
-    type = Column(String, nullable=False)
-    value_bool = Column(Boolean)
-    value_date = Column(DateTime)
-    value_number = Column(Float)
-    value_string = Column(String)
-    is_bool = Column(Boolean, default=False)
-    is_date = Column(Boolean, default=False)
-    is_number = Column(Boolean, default=False)
-    is_string = Column(Boolean, default=False)
-    is_multistring = Column(Boolean, default=False)
-    element_id = Column(Integer, ForeignKey("elements.id", ondelete="CASCADE"))
+    version = Column(String, nullable=False)
+    is_yanked = Column(Boolean, default=False)
+    package_id = Column(Integer, ForeignKey("packages.id", ondelete="CASCADE"))
 
-    element = relationship("Element", back_populates="attributes")
+    package = relationship("Package", back_populates="releases")
