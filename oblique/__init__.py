bf92e5369d6900dbe16c12668c9538abfca6de22
Astariul <remondnicola@gmail.com>
2023-09-16 23:16:40 +0900

    ✨ Add constant for the place where components are stored

--- oblique/__init__.py
+++ oblique/__init__.py
@@ -1,7 +1,10 @@
-"""Dummy package content for `pytere`."""
+"""Root of the `oblique` package."""
+import pathlib
 
 
+COMPONENTS_DIR = pathlib.Path(__file__).parent.resolve() / "components"
 __version__ = "1.0.0.dev0"
+
 
 # isort: off
 
